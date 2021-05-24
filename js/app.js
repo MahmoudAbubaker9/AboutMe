6464d10993d12d415a2b36b420cd2cc2b3621607
mahmoudabubaker9 <m7moud.abubaker.92@gmail.com>
2021-05-24 16:25:14 +0300

    add final edit

--- js/app.js
+++ js/app.js
@@ -10,10 +10,16 @@ myName = myName.toLowerCase()
 console.log(myName)
 switch(myName) {
     case 'yes':
+    case 'y':    
         alert('Nice ' + userName)
         break;
     case 'no':
+    case 'n':
         alert('my name is mahmoud abubaker')
+        break;
+    default:
+        alert('my name is mahmoud abubaker')
+        
 }
 //console.log(myName)
 
@@ -22,10 +28,15 @@ let prog = prompt('did you like programing?')
 prog = prog.toLowerCase()
 switch(prog) {
     case 'yes':
+    case 'y':
         alert('nice me too')
         break;
     case 'no':
+    case 'n':
         alert('It is a good and fun thing to try it')
+        break;
+    default:
+        alert('Please enter Yes or No')
 }
 //console.log(prog)
 
@@ -33,10 +44,15 @@ let myDesgin = prompt('Do you like the design??')
 myDesgin = myDesgin.toLowerCase()
 switch(myDesgin) {
     case 'yes':
+    case 'y':
         alert('Well, I can design with multiple programs like Photoshop, its my hobby')
         break;
     case 'no':
+    case 'n':
         alert('It is a good and fun thing to try it')
+        break;
+    default:
+        alert('Please enter Yes or No')
 }
 //console.log(myDesgin)
 
@@ -44,10 +60,15 @@ let myMovies = prompt('Do you like the Movies or Series??')
 myMovies = myMovies.toLowerCase()
 switch(myMovies) {
     case 'yes':
+    case 'y':
         alert('It is a good thing, about myself, I prefer series more than movies (the best series for me The mentalist)')
         break;
     case 'no':
+    case 'n':
         alert('It is a good and fun thing to try it')
+        break;
+    default:
+        alert('Please enter Yes or No')
 }
 
 //console.log(myMovies)
@@ -56,10 +77,16 @@ let myAge = prompt('Last question!! did you know my age?')
 myAge = myAge.toLowerCase()
 switch(myAge) {
     case 'yes':
+    case 'y':
         alert('nice ' + userName)
         break;
     case 'no':
+    case 'n':
         alert('my age is 28 ')
+        break;
+    default:
+        alert('Please enter Yes or No')
+        
 }
 //console.log(myAge)
 
